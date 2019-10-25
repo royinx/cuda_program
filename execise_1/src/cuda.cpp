6fcd37546cb732f2b9826a5272a5fa89033b0d5e
royinx <royfork@hotmail.com>
2019-10-25 15:42:26 +0800

    cuda.cpp

--- execise_1/src/cuda.cpp
+++ execise_1/src/cuda.cpp
@@ -1,7 +1,40 @@
-__global__ void VecAdd(float *A , float* B, float* C){
-    int i = threadIdx.x;
-    C[i] = A[i] + B[i];
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+bool InitCUDA()
+{
+    int count;
+
+    cudaGetDeviceCount(&count);
+    if(count == 0) {
+        fprintf(stderr, "There is no device.\n");
+        return false;
+    }
+
+    int i;
+    for(i = 0; i < count; i++) {
+        cudaDeviceProp prop;
+        if(cudaGetDeviceProperties(&prop, i) == cudaSuccess) {
+            if(prop.major >= 1) {
+                break;
+            }
+        }
+    }
+
+    if(i == count) {
+        fprintf(stderr, "There is no device supporting CUDA 1.x.\n");
+        return false;
+    }
+
+    cudaSetDevice(i);
+
+    return true;
 }
-int main(){
-    VecAdd<<<1,N>>>(A, B, C);
-}
+
+// __global__ void VecAdd(float *A , float* B, float* C){
+//     int i = threadIdx.x;
+//     C[i] = A[i] + B[i];
+// }
+// int main(){
+//     VecAdd<<<1,N>>>(A, B, C);
+// }
